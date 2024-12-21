f1400bf548ac1fd9dbdb397b27989db72cd6c2af
Rohit <rohitk14458@gmail.com>
2024-12-22 00:37:42 +0530

    error solved

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,9 @@ import { Button } from '@/components/ui/button'
 function App() {
   return (
     <>
-<<<<<<< Updated upstream
-      <h1 className='text-3xl font-bold '># Welcome to <a target='_blank' href="http://vrblancer.com">vrblancer.com</a></h1>
-=======
       <h1 className='text-3xl font-bold '># Welcome to <a target='_blank' href="http://atxlancer.com">Atxlancer.com</a></h1>
       <p className="mt-2 ">This is our new freelancing website</p>
       <Button className='mt-4'>Login</Button>
->>>>>>> Stashed changes
     </>
   )
 }
