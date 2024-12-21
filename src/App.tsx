5761c136f44455403caa2d86c249e45100e562f0
Bilal Faiz <bilal.idrishi261479@gmail.com>
2024-12-21 22:44:24 +0530

    folder setup 21/12/24

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
     <>
       <h1 className='text-3xl font-bold '># Welcome to <a target='_blank' href="http://vrblancer.com">vrblancer.com</a></h1>
     
-    <p className="mt-2 ">This is our new freelancing website</p></>
+       <p className="mt-2 ">This is our new freelancing website</p>
+       
+    </>
   )
 }
 
