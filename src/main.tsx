ec74d5a53635e6b4c9c2d62ff787bb7ed811ec30
Bilal Faiz <bilal.idrishi261479@gmail.com>
2024-12-24 01:35:31 +0530

    Add toggle button

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import ThemeProvider from "./context/dark-mode/them-provider.tsx";
+import ThemeProvider from "./context/dark-mode/theme-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
