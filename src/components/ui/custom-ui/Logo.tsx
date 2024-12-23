e4f96520c6859e448ce75e577c65809c37958ef3
Rohit <rohitk14458@gmail.com>
2024-12-24 01:56:53 +0530

    navigation modify

---
+++ src/components/ui/custom-ui/Logo.tsx
@@ -0,0 +1,10 @@
+
+const Logo = () => {
+    return (
+        <div className="font-atxlancer_secondary text-xl font-bold">
+            <span className="text-atxlancer_them_color">Atx</span>Lancer
+        </div>
+    )
+}
+
+export default Logo
